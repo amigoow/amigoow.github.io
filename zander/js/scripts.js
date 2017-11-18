0aa86efe67be540cab277edcb428d203d31da0b1
Faisal <info@pilates1901.com>
2017-11-19 03:10:27 +0500

    zand added

---
+++ zander/js/scripts.js
@@ -0,0 +1,5 @@
+// Empty JS for your own code to be here
+$('.play').click(function() {
+  video = '<iframe width="100%" height="281" frameborder="0" src="' + $('img').attr('data-video') + '"></iframe>';
+  $('.video').replaceWith(video);
+});
