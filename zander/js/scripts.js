f759de07e22b21b59550e39fb77a45fb161504dd
Faisal <info@pilates1901.com>
2017-12-01 19:42:17 +0500

    Slider added

--- zander/js/scripts.js
+++ zander/js/scripts.js
@@ -3,3 +3,17 @@
 //   video = '<iframe width="100%" height="281" frameborder="0" src="' + $('img').attr('data-video') + '"></iframe>';
 //   $('.video').replaceWith(video);
 // });
+
+var totalSteps = 4;
+
+$('#program-carousel').carousel();
+$('#program-carousel').on('slide.bs.carousel', function (e) {
+  
+  //update progress
+  var step = $(e.relatedTarget).data('step');
+  var percent = (parseInt(step) / totalSteps) * 100;
+  
+  $('.progress-bar').css({width: percent + '%'});
+  $('.progress-bar').text("Step " + step);
+  
+})
