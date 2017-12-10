6db98f3e61ce206ea32dc1f6812320b86197b33c
Faisal <info@pilates1901.com>
2017-12-10 22:26:58 +0500

    settimeout

--- zander/js/scripts.js
+++ zander/js/scripts.js
@@ -4,48 +4,50 @@
 //   $('.video').replaceWith(video);
 // });
 
+setTimeout(function(){
+	var totalSteps = 4;
 
-var totalSteps = 4;
+	$('#program-carousel').carousel();
+	$('#program-carousel').on('slide.bs.carousel', function (e) {
+	  
+	  //update progress
+	  var step = $(e.relatedTarget).data('step');
+	  var percent = (parseInt(step) / totalSteps) * 100;
+	  
+	  $('.progress-bar').css({width: percent + '%'});
+	  $('.progress-bar').text("Step " + step);
+	  
+	});
 
-$('#program-carousel').carousel();
-$('#program-carousel').on('slide.bs.carousel', function (e) {
-  
-  //update progress
-  var step = $(e.relatedTarget).data('step');
-  var percent = (parseInt(step) / totalSteps) * 100;
-  
-  $('.progress-bar').css({width: percent + '%'});
-  $('.progress-bar').text("Step " + step);
-  
-});
+	var clock;
 
-var clock;
-
-$(document).ready(function () {
+	$(document).ready(function () {
 
 
 
-    clock = $('.clock').FlipClock(10*60, {
-        clockFace: 'MinuteCounter',
-        countdown: true
-    });
-});
+	    clock = $('.clock').FlipClock(10*60, {
+	        clockFace: 'MinuteCounter',
+	        countdown: true
+	    });
+	});
 
-$(".testimonials").iziModal({
-    history: false,
-    iframe : true,
-    fullscreen: true,
-    group: 'testimonials',
-    loop: true,
-    headerColor: '#29aeb1'
-});
-$(".top-header-modal").iziModal({
-    history: false,
-iframe : true,
-fullscreen: true,
+	$(".testimonials").iziModal({
+	    history: false,
+	    iframe : true,
+	    fullscreen: true,
+	    group: 'testimonials',
+	    loop: true,
+	    headerColor: '#29aeb1'
+	});
+	$(".top-header-modal").iziModal({
+	    history: false,
+	iframe : true,
+	fullscreen: true,
 
-});
-$(".action-plan li").click(function(){
-  $(".action-plan li").removeClass("active");
-  $(this).addClass("active");
-});
+	});
+	$(".action-plan li").click(function(){
+	  $(".action-plan li").removeClass("active");
+	  $(this).addClass("active");
+	});
+}, 2000);
+
