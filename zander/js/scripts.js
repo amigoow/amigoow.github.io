f3d91b038b8baefeda9a341c0f6806dec474f478
Faisal <info@pilates1901.com>
2017-12-15 00:14:20 +0500

    count down timer

--- zander/js/scripts.js
+++ zander/js/scripts.js
@@ -50,6 +50,26 @@ $(window).on('load', function(){
 	  $(this).addClass("active");
 	});
 
+	function startTimer(duration, display) {
+	    var timer = duration, minutes, seconds;
+	    setInterval(function () {
+	        minutes = parseInt(timer / 60, 10)
+	        seconds = parseInt(timer % 60, 10);
+
+	        minutes = minutes < 10 ? "0" + minutes : minutes;
+	        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+	        display.textContent = minutes + ":" + seconds;
+
+	        if (--timer < 0) {
+	            timer = duration;
+	        }
+	    }, 1000);
+	}
+
+	
+    var fiveMinutes = 60 * 10, display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
 });
 
 // setTimeout(function(){
